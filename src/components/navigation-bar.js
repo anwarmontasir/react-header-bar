e89573e0537ceec5e3a54babb69d18b40a73e94b
Anwar Montasir <anwarmontasir@gmail.com>
2018-11-28 12:24:54 -0800

    complete nav bar

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 import './navigation-bar.css';
 
-export default function NavigationBar() {
+export default function NavigationBar(props) {
+    const links = props.links.map((link, index) => (
+        <li key={index}>
+            <a href={link.href}>
+                {link.text}
+            </a>
+        </li>
+    ));
     return (
-
+        <nav className="navigation-bar">
+          <h1>{props.title}</h1>
+          <ul className="navigation-bar-nav">
+            {links}
+          </ul>
+        </nav>
     );
 }
